63a137cd9fd45d3bff7ca03a68debe44609b2ed5
peshelg <peshelg@afrosoft.co.zw>
2024-07-20 10:06:07 +0200

    Floor name

--- components/topNav.jsx
+++ components/topNav.jsx
@@ -5,7 +5,8 @@ import React from 'react';
 const TopNav = () => {
     return (
         <div className="w-full top-0 sticky text-sm bg-white z-50 p-2 text-green-700 border-b flex flex-row items-center justify-between">
-        <Link href={'/'} className="font-bold flex flex-row items-center gap-x-1"><Image className="rounded" alt='logo' src={'/assets/images/logo.png'} width={40} height={40} loading="lazy"/>Tobbaco Market Index</Link>
+          <div className='w-[1200px] mx-auto flex flex-row gap-x-4 justify-between items-center'>
+          <Link href={'/'} className="font-bold flex flex-row items-center gap-x-1"><Image className="rounded" alt='logo' src={'/assets/images/logo.png'} width={40} height={40} loading="lazy"/>Tobbaco Market Index</Link>
         <ul className="flex flex-row gap-x-2 items-center">
         <li className='text-gray-700 hover:to-green-500 max-md:hidden'><Link href={'/'} className='px-2'>Home</Link></li>
         <li className='text-gray-700 hover:to-green-500 max-md:hidden'><Link href={'/market'} className='px-2'>Market</Link></li>
@@ -18,6 +19,8 @@ const TopNav = () => {
               <li><Link href={'/auth/sign-in'}>Login</Link></li>
               <li><Link href={'/auth/sign-up'} className="bg-green-700 p-2 rounded-md text-white">Sign Up</Link></li>
             </ul>
+          </div>
+        
           </div>
     );
 }
